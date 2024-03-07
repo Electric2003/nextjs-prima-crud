823d41aada12433d16abae435ca9634ce3854aae
Matias Ingaglio <mati.ingaglio@gmail.com>
2024-03-07 17:08:54 -0300

    parte de front

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,27 @@
-function HomePage() {
+import { prisma } from '@/app/libs/prisma'
+
+async function loadTasks() {
+  // const res = await fetch('http://localhost:3000/api/tasks')
+  // const data = await res.json()
+
+  return await prisma.task.findMany()
+}
+
+async function HomePage() {
+
+  const tasks = await loadTasks()
+  console.log(tasks);
+
   return (
-    <div>Tareas</div>
+    <section className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-3 mt-10'>{tasks.map(task => (
+        <div key={task.id} className='bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer'>
+          <h3 className='font-bold text-2xl mb-2'>{task.title}</h3>
+          <p>{task.description}</p>
+          <p className='mt-2'>{new Date(task.createdAt).toLocaleDateString()}</p>
+        </div>
+      ))}</div>
+    </section>
   )
 }
 
